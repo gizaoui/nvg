eab759e81e68b0587337a9c38c8f20f73b773773
Gilles ZAOUI <gizaoui@gmail.com>
2026-09-07 12:41:39 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,39 +1,36 @@
-public class MaximumSubarray {
+public class MaxSubarrayFixedSize {
 
-    public static int[] maxSubarrayIndices(int[] nums) {
-        if (nums == null || nums.length == 0) {
+    public static int[] maxSubarrayIndicesFixedSize(int[] nums, int k) {
+        if (nums == null || nums.length < k || k <= 0) {
             return new int[]{};
         }
 
-        int maxSoFar = nums[0];
-        int maxEndingHere = nums[0];
+        // Calcul de la somme de la première fenêtre
+        int currentSum = 0;
+        for (int i = 0; i < k; i++) {
+            currentSum += nums[i];
+        }
 
-        int start = 0;
+        int maxSum = currentSum;
         int bestStart = 0;
-        int bestEnd = 0;
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > maxEndingHere + nums[i]) {
-                maxEndingHere = nums[i];
-                start = i;
-            } else {
-                maxEndingHere += nums[i];
-            }
-
-            if (maxEndingHere > maxSoFar) {
-                maxSoFar = maxEndingHere;
-                bestStart = start;
-                bestEnd = i;
+        // Glissement de la fenêtre sur le reste du tableau
+        for (int i = k; i < nums.length; i++) {
+            currentSum += nums[i] - nums[i - k];
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                bestStart = i - k + 1;
             }
         }
 
-        return new int[]{bestStart, bestEnd};
+        return new int[]{bestStart, bestStart + k - 1};
     }
 
     public static void main(String[] args) {
         int[] tableau = {10, 20, 30};
-        int[] indices = maxSubarrayIndices(tableau);
+        int k = 2; // Taille de la sous-suite recherchée
 
-        System.out.println("Indices : [" + indices[0] + ", " + indices[1] + "]");
+        int[] indices = maxSubarrayIndicesFixedSize(tableau, k);
+        System.out.println("Indices pour k=" + k + " : [" + indices[0] + ", " + indices[1] + "]");
     }
 }
