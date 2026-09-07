3250c18d0520c5a2192290cc908f41c947f25001
Gilles ZAOUI <gizaoui@gmail.com>
2026-09-07 14:21:38 +0200

    Create reshape.java

---
+++ reshape.java
@@ -0,0 +1,24 @@
+
+    public static String reshape(int n, String str) {
+
+        String cleanStr="";
+        for(char c : str.toCharArray()) {
+            if(c!=` `) {
+                cleanStr+=c;
+            }
+        }
+
+        String result = "";
+        for(int i=0; i<cleanStr.length(); ++i) {
+            if(i>0 && i%n==0) {
+                result+=`\n`;
+            }
+            result+=cleanStr.toCharArray()[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+       System.out.println(reshape(3, "abc de fghi j"));
+    }
