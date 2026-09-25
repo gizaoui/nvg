5f57fe26d52a5b023d61731ae9b3b7ad82318589
Gilles ZAOUI <gizaoui@gmail.com>
2026-09-25 12:21:17 +0200

    Update MaxProfit.go

--- MaxProfit.go
+++ MaxProfit.go
@@ -1,52 +1,47 @@
+package main
+
 // Soit un tableau d'entiers représentant les bénéfices par mois.
 // On souhaite connaître la plage de mois consécutif enregistrant le plus grand bénéfice.
 // go run MaxProfit.go 
 // go build -o MaxProfit MaxProfit.go && ./MaxProfit
 
-package main
 
 import "fmt"
 
-type MaxProfit struct{}
 
-func (m MaxProfit) MaxProfitIndices(nums []int) []int {
-	if len(nums) == 0 {
+func MaxProfit(nums []int, k int) []int {
+	if nums == nil || len(nums) < k || k <= 0 {
 		return []int{}
 	}
 
-	maxSoFar := nums[0]
-	maxEndingHere := nums[0]
+	// Calcul de la somme de la première fenêtre
+	currentSum := 0
+	for i := 0; i < k; i++ {
+		currentSum += nums[i]
+	}
 
-	start := 0
+	maxSum := currentSum
 	bestStart := 0
-	bestEnd := 0
 
-	for i := 1; i < len(nums); i++ {
-		if nums[i] > maxEndingHere+nums[i] {
-			maxEndingHere = nums[i]
-			start = i
-		} else {
-			maxEndingHere += nums[i]
-		}
-
-		if maxEndingHere > maxSoFar {
-			maxSoFar = maxEndingHere
-			bestStart = start
-			bestEnd = i
+	// Glissement de la fenêtre sur le reste du tableau
+	for i := k; i < len(nums); i++ {
+		currentSum += nums[i] - nums[i-k]
+		if currentSum > maxSum {
+			maxSum = currentSum
+			bestStart = i - k + 1
 		}
 	}
 
-	return []int{bestStart, bestEnd}
+	return []int{bestStart, bestStart + k - 1}
 }
 
-// Résultat : Indices pour k=2 : [4,5] -> 3+8=11
 func main() {
-	tableau := []int{1,2,3,4,3,8,3,5,1} // Liste des bénéfices de janvier à septembre.
-	
-	ms := MaxProfit{}
-	indices := ms.MaxProfitIndices(tableau)
+	tableau := []int{1, 2, 3, 4, 3, 8, 3, 5, 1} // Liste des bénéfices de janvier à septembre.
+	k := 2                                       // Taille de la sous-suite recherchée
 
-	if len(indices) == 2 { // 2 : Taille de la sous-suite recherchée
-		fmt.Printf("Indices : [%d, %d]\n", indices[0], indices[1])
+	indices := MaxProfit(tableau, k)
+
+	if len(indices) == 2 {
+		fmt.Printf("Indices pour k=%d : [%d, %d]\n", k, indices[0], indices[1])
 	}
 }
